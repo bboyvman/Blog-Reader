321adc3f50df84a677720d194459754d389612e3
VartanArtinian <bboyvartan@gmail.com>
2014-10-16 13:54:38 -0700

    vid 9

--- app/src/main/java/andriod/artinianv/blogreader/BlogPostTask.java
+++ app/src/main/java/andriod/artinianv/blogreader/BlogPostTask.java
@@ -4,9 +4,17 @@ import android.os.AsyncTask;
 
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
+
 public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject> {
     @Override
-    protected JSONObject doInBackground(Activity... params) {
-        return null;
+    protected JSONObject doInBackground(Activity... activities) {
+        try {
+            URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=10");
+        }
+        catch(MalformedURLException error) {
+            Log.e("BlogPostTask", "Malformed URL:" + error);
+        }
+            return null;
     }
 }
